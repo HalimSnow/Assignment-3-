1229d54c6586a4292bf8650417cc6b26d4468ce6
sn0w <halimt0625@gmail.com>
2025-09-26 17:38:51 -0400

    test: add Ping test using MongoDB Testcontainer

--- MongoDBConnector.Test.cs/UnitTest1.cs
+++ MongoDBConnector.Test.cs/UnitTest1.cs
@@ -1,10 +1,35 @@
-﻿namespace MongoDBConnector.Test.cs;
+﻿using Xunit;   
+using MongoDBConnector.cs; 
+using System.Threading.Tasks; 
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Builders;
+
+
+namespace MongoDBConnector.Test.cs;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        var mongoDbContainer = new TestcontainersBuilder<TestcontainersContainer>()
+            .WithImage("mongo:6.0") // official MongoDB image
+                .WithCleanUp(true)
+                .WithPortBinding(27017, true) // random free port
+                .Build();
 
+        await mongoDbContainer.StartAsync();
+
+        var connectionString = $"mongodb://localhost:{mongoDbContainer.GetMappedPublicPort(27017)}";
+        var connector = new MongoDBConnector(connectionString);
+
+        // Act
+        var result = connector.Ping();
+
+        // Assert
+        Assert.True(result);
+
+        // Clean up
+        await mongoDbContainer.StopAsync();
     }
 }
--- MongoDBConnector.cs/Class1.cs
+++ MongoDBConnector.cs/Class1.cs
@@ -1,6 +1,32 @@
-﻿namespace MongoDBConnector.cs;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
-public class Class1
+namespace MongoDBConnector.cs;
+
+public class MongoDBConnector
 {
+    private readonly MongoClient _client;
 
+
+    public MongoDBConnector(string connectionString)
+    {
+        _client = new MongoClient(connectionString);
+    }
+    public bool Ping()
+    {
+        try
+        {
+            var database = _client.GetDatabase("admin");
+
+            var command = new BsonDocument("ping", 1);
+            var result = database.RunCommand<BsonDocument>(command);
+
+            return result != null;
+
+        }
+        catch
+        {
+            return false;
+        }
+    }
 }
